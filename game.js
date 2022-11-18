6f3fd39c250e99af77763ea71f1369363c5ca42d
millyexe <milenalobko18@outlook.com>
2022-11-18 01:09:57 -0500

    first

---
+++ game.js
@@ -0,0 +1,28 @@
+import {
+  update as updateSnake,
+  draw as drawSnake,
+  SNAKE_SPEED,
+} from "./snake.js";
+
+let lastRenderTime = 0;
+
+function maim(currentTime) {
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+
+  console.log("render");
+  lastRenderTime = currentTime;
+
+  update();
+  draw();
+}
+
+window.requestAnimationFrame(maim);
+
+function update() {
+  updateSnake();
+}
+
+function draw() {
+  drawSnake;
+}
